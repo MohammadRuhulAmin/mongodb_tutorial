9e16bc88d5fd71ee38b94e131d5943455120844e
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-22 12:30:51 +0600

    multiple sort

--- Query/sort.js
+++ Query/sort.js
@@ -13,3 +13,16 @@ var sort_name = db.getCollection('persons').find({}).sort({"name":1});
 /**print all the data in descending sorted order */
 
 var sort_name = db.getCollection('persons').find({}).sort({"name":-1});
+
+/**Multiple sort: */
+
+var msort = db.getCollection('persons')
+.find({
+    $and:[
+        {"gender":"female"},
+        {"eyeColor":"green"},
+        {"age":{"$lt":27}},
+        {"age":{"$gt":23}},
+        {"favoriteFruit":{"$in":["banana","orange"]}}
+    ]
+}).sort({"age":-1},{name:1},{gender:-1});
