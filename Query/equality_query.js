805123eb3009ebb9105db03f29936a542c305984
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-22 10:48:50 +0600

    equal_query added

---
+++ Query/equality_query.js
@@ -0,0 +1,15 @@
+/**To find json by values: */
+/*
+    {"name":"kitty snow"}
+    {"age":39}
+    {"gender":"female","eyeColor":"blue"}
+    Here , is considered as and condition
+*/
+
+get_json_has_age_20 = db.getCollection('collection_name').find({"age":20});
+
+get_json_by_name = db.getCollection('collection_name').find({name:"Aurelia Gonzales"})
+
+/** Applying And in Query */
+get_json_gender_female_and_eye_color_green = db.getCollection('persons').
+                                            find({gender:"female",eyeColor:"green"});
