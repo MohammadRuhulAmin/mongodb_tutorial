421eed156cca44d14b22a3571ba15191ebd2c8dc
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-06-23 16:20:19 +0600

    updateMany()

--- update_record/update_many.js
+++ update_record/update_many.js
@@ -1,4 +1,11 @@
 /**
- * 
+ * For updating multiple records we use updateMany() function
+ * db.collection.updateMany({ status: 'new' }, { $inc: { views: 1 } });
  * 
  */
+
+db.collection('users').updateMany(
+    {status:'inactive'}, /**filter data */
+    {$inc:{views:3}}, /** increment views field with views += 3 */
+    {upsert:false} /** for the option */
+);
